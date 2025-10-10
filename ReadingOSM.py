7f455645e417f9d8ef4b51639068a4aa8609e959
corvino <corvino1342@gmail.com>
2025-10-10 15:04:52 +0200

    small changes on the saving of dataset

--- ReadingOSM.py
+++ ReadingOSM.py
@@ -2,13 +2,13 @@ import osmnx as ox
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-
-gdf = gpd.read_file("avigliano2.osm", layer='multipolygons')
+place = 'avigliano3'
+gdf = gpd.read_file(f"{place}Footprints.osm", layer='multipolygons')
 buildings = gdf[gdf["building"].notnull()].copy()
 
 print(buildings.other_tags)
 
 
 buildings.plot(figsize=(8, 8))
-plt.title("Buildings in Avigliano")
-plt.show()
+plt.title(f"Buildings in {place}")
+plt.savefig(f"{place}Footprints.png")
