b2270ae1c1496d92b327736d9138a3ccedd39c11
corvino <corvino1342@gmail.com>
2025-10-10 15:36:36 +0200

    the actual images and labels are quite easy to use

--- MassachusettsBuidingDataset.py
+++ MassachusettsBuidingDataset.py
@@ -1,11 +1,16 @@
-# %%
 from PIL import Image
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# %%
-
 image = Image.open('datasets/massachusetts-buildings-dataset/tiff/train/22678915_15.tiff')
+
+# Plot the image
+plt.imshow(image)
+plt.axis('off')  # optional, hides the axes
+plt.show()
+
+
+image = Image.open('datasets/massachusetts-buildings-dataset/tiff/train_labels/22678915_15.tif')
 
 # Plot the image
 plt.imshow(image)
